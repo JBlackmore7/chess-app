59d1bfdbf74ac63b9b36d5d256f2f757cbb5cb29
Jared Blackmore <jblackmore17@gmail.com>
2022-11-26 09:35:53 -0330

    logic/AI update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import "./App.css";
 import { useState } from "react";
-import { Chess } from "chess.js";
+import { Chess } from "./Chess";
 import { Chessboard } from "react-chessboard";
+import ChessEngine from "./Engine";
 
 function App() {
   const [game, setGame] = useState(new Chess());
 
-  function makeAMove(move) {
-    const gameCopy = { ...game };
-    const result = gameCopy.move(move);
-    setGame(gameCopy);
-    return result; // null if the move was illegal, the move object if the move was legal
-  }
-
-  function makeRandomMove() {
-    const possibleMoves = game.moves();
-    if (game.game_over() || game.in_draw() || possibleMoves.length === 0) return; // exit if the game is over
-    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
-    makeAMove(possibleMoves[randomIndex]);
-  }
-
-  function onDrop(sourceSquare, targetSquare) {
-    const move = makeAMove({
-      from: sourceSquare,
-      to: targetSquare,
-      // promotion: "q", // always promote to a queen for example simplicity
-    });
-
-    // illegal move
-    if (move === null) return false;
-
-    setTimeout(makeRandomMove, 200);
-    return true;
-  }
-
   console.log(game.pgn());
-  return <Chessboard position={game.fen()} onPieceDrop={onDrop} />;
+  return <ChessEngine game={game} setGame={setGame} />;
 }
 
 export default App;
