8bd99047d8afdf087de318fc5361e45a306a26d0
Jared Blackmore <jblackmore17@gmail.com>
2023-03-19 21:50:07 -0230

    imports

--- src/components/ChessHome.js
+++ src/components/ChessHome.js
@@ -1,7 +1,7 @@
 import "../App.css";
 import styles from "./ChessHome.module.css";
 import { useState, useRef } from "react";
-import { Chess } from "../Chess";
+import * as Chess from "../Chess";
 import ChessEngine from "../Engine";
 import { useSize } from "./hooks";
 import { useModal } from "./hooks";
