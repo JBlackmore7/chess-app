3175f18fb1d8d90617b01d43cd6e86100620f531
Jared Blackmore <jblackmore17@gmail.com>
2023-04-22 14:54:14 -0230

    server config

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
+// import postgres from "postgres";
+
 const express = require("express");
 const cors = require("cors");
+// const sql = postgres({
+//   /* options */
+// }); // will use psql environment variables
 
 const app = express();
+const chessProfile = {
+  name: "Jared",
+  difficulyLevel: "Intermediate",
+  username: "Jar",
+  email: "555@fake.com",
+};
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/message", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json({ message: chessProfile });
 });
 
 app.listen(8000, () => {
